c57af6b716ce20ddc7ba9c395dad3887917319f2
secretagentman01 <amonbessman1@yahoo.com>
2016-02-21 22:54:33 -0500

    Want to push this to GitHub

--- New_Experiment_Project/Program.cs
+++ New_Experiment_Project/Program.cs
@@ -14,7 +14,7 @@ namespace New_Experiment_Project
         /// <param name="args"></param>
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("I love learning new thing");
         }
     }
 }
