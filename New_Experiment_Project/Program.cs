3e112257e7a53b2a3d7310a858df8953d8ec78ec
secretagentman01 <amonbessman1@yahoo.com>
2016-02-21 22:46:20 -0500

    Initial Comment

---
+++ New_Experiment_Project/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace New_Experiment_Project
+{
+    class Program
+    {
+        /// <summary>
+        /// This is for testing purposes. Just want to know how thing will turn out
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+        }
+    }
+}
