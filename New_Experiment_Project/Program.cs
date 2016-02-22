dba0f851e9c192a29fdac66deb295ddcab15813c
secretagentman01 <amonbessman1@yahoo.com>
2016-02-21 22:48:33 -0500

    Localize en

--- New_Experiment_Project/Program.cs
+++ New_Experiment_Project/Program.cs
@@ -14,7 +14,7 @@ namespace New_Experiment_Project
         /// <param name="args"></param>
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello");
+            Console.WriteLine("Hello Y'all");
         }
     }
 }
